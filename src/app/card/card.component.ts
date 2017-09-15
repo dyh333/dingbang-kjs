939c33b4713f5a1be67192c22821da613bf922a4
unknown <zhangz@sipsd.local>
2017-09-15 14:47:20 +0800

    新增页面头部组件

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { CardService } from './card.service';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+private _cardData: Object;
+  constructor(private service: CardService) { }
+
+  ngOnInit() {
+    this.service.loadCard().subscribe({
+      next: (cardData) => {
+        console.log(cardData);
+        this._cardData = cardData;
+      }
+    });
+  }
+
+}

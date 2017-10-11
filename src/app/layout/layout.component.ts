05100d237a502d1df495ca34eca102f243047f6d
dingyh <dyh333@gmail.com>
2017-10-11 18:38:06 +0800

    修改图片资源

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class LayoutComponent implements OnInit {
-    array = [ 1, 2, 3, 4, 5 ];
+    array = [ 1, 2, 3 ];
 
     ngOnInit() { 
         

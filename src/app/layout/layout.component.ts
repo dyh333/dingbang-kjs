9f173612efb070400db6e11be5e5f9db96065aeb
ZhangZheng <zhangz@sipsd.local>
2017-10-13 15:27:13 +0800

    修改页面跳转左侧和头部菜单联动问题

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router, NavigationEnd, RouteConfigLoadStart } from "@angular/router";
+import { MenuService } from '../shared/components/menu/menu.service';
 @Component({
     selector: 'layout-component',
     templateUrl: './layout.component.html',
     styleUrls: ['./layout.component.scss']
+    // providers: [MenuService]
 })
 
 export class LayoutComponent implements OnInit {
-    array = [ 1, 2, 3 ];
+    array = [1, 2, 3];
 
-    ngOnInit() { 
-        
+    ngOnInit() {
+
     }
 }
--- src/app/shared/components/menu/menu.service.ts
+++ src/app/shared/components/menu/menu.service.ts
@@ -7,10 +7,24 @@ import 'rxjs/add/observable/throw';
 @Injectable()
 export class MenuService {
     constructor(private httpClient: HttpClient) { }
-    
-    loadMenu(url: string): Observable<any>{
+
+    loadMenu(url: string): Observable<any> {
         return this.httpClient.get(url).catch((err: any) => {
             return Observable.throw(err);
         });
+    }
+
+
+    setSelected(data, url: string) {
+        if (!url) { return; }
+        _.forEach(data, (item) => {
+            item.selected = false;
+        });
+        if (_.find(data, function (o) { return o.path === url; }) === undefined) {
+            data[0].selected = true;
+        } else {
+            _.find(data, function (o) { return o.path === url; }).selected = true;
+
+        }
     }
 }
